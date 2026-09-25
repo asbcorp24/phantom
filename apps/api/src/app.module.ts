68aaf8c1e61928cb9c66d72659c79d7d433ae4ce
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-25 21:55:18 +0300

    feat(company): register departments module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,5 +4,6 @@ import { HealthController } from './health.controller';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-@Module({imports:[PrismaModule,AuthModule,OrganizationsModule,UsersModule],controllers:[HealthController]})
+import { DepartmentsModule } from './departments/departments.module';
+@Module({imports:[PrismaModule,AuthModule,OrganizationsModule,UsersModule,DepartmentsModule],controllers:[HealthController]})
 export class AppModule {}
